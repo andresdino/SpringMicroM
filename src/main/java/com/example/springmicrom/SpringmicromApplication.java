72829a5eec99022e9a9081953c4b9ef92ce359f8
andresdino <andresdino9ster@gmail.com>
2024-08-14 07:30:46 -0500

    Mi primer commit

---
+++ src/main/java/com/example/springmicrom/SpringmicromApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmicrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmicromApplication.class, args);
+	}
+
+}
